fa73ce6b9dccc2e0a97fff7f2f692d7f2f6d1134
Bayel <bayel.durusbekov.kk@gmail.com>
2022-04-26 17:35:05 +0600

    fix

--- src/components/AuthComponent.js
+++ src/components/AuthComponent.js
@@ -1,99 +1,86 @@
 import React from 'react';
 import {StyleSheet, View} from "react-native";
 import {Button, Paragraph, TextInput} from "react-native-paper";
-<<<<<<< HEAD
 import {useNavigation} from "@react-navigation/native";
 
 const AuthComponent = () => {
     const {navigate} = useNavigation();
 
-=======
-import {useNavigation} from '@react-navigation/native'
+        return (
+            <View style={styles.container}>
+                <View style={{flexDirection: 'row', marginVertical: 10, justifyContent: 'center'}}>
+                    <Paragraph style={styles.myBtnEmail}>
+                        Электронная почта
+                    </Paragraph>
+                    <Paragraph style={styles.myBtnEmailRight}>
+                        Номер телефона
+                    </Paragraph>
+                </View>
+                <TextInput
+                    label="Электронная почта"
+                    // value={full_name}
+                    // onChangeText={t => setFullName(t)}
+                    style={styles.myInput}
+                />
+                <TextInput
+                    label="Пароль"
+                    // value={phone_number}
+                    // keyboardType={'phone-pad'}
+                    // onChangeText={t => setPhoneNumber(t)}
+                    style={styles.myInput}
+                    secureTextEntry
+                    right={<TextInput.Icon name="eye"/>}
+                />
+                <Button style={styles.myButton} mode="contained" onPress={() => console.log('Pressed')}>
+                    Войти
+                </Button>
+                <Paragraph style={styles.myParagraph} onPress={() => navigate('AuthForgotPass')}>Забыли пароль?</Paragraph>
+                <Paragraph style={styles.myParagraph} onPress={() => navigate('AuthRegistrationScreen')}>Зарегистрироваться</Paragraph>
+            </View>
+        );
+    };
 
-const AuthComponent = ({navigation}) => {
-    const {navigate} = useNavigation()
->>>>>>> a4a9f50c282f5562ff02c12ce03d699289675385
-    return (
-        <View style={styles.container}>
-           <View style={{flexDirection: 'row', marginVertical: 10, justifyContent: 'center'}}>
-               <Paragraph  style={styles.myBtnEmail}>
-                   Электронная почта
-               </Paragraph>
-               <Paragraph  style={styles.myBtnEmailRight}>
-                   Номер телефона
-               </Paragraph>
-           </View>
-            <TextInput
-                label="Электронная почта"
-                // value={full_name}
-                // onChangeText={t => setFullName(t)}
-                style={styles.myInput}
-            />
-            <TextInput
-                label="Пароль"
-                // value={phone_number}
-                // keyboardType={'phone-pad'}
-                // onChangeText={t => setPhoneNumber(t)}
-                style={styles.myInput}
-                secureTextEntry
-                right={<TextInput.Icon name="eye" />}
-            />
-            <Button style={styles.myButton} mode="contained" onPress={() => console.log('Pressed')}>
-                Войти
-            </Button>
-<<<<<<< HEAD
-            <Paragraph style={styles.myParagraph}  onPress={() => navigate('AuthForgotPass')}>Забыли пароль?</Paragraph>
-            <Paragraph style={styles.myParagraph}>Зарегистрироваться</Paragraph>
-=======
-            <Paragraph style={styles.myParagraph}>Забыли пароль?</Paragraph>
-            <Paragraph style={styles.myParagraph} onPress={() => navigate('AuthRegistrationScreen')}>Зарегистрироваться</Paragraph>
->>>>>>> a4a9f50c282f5562ff02c12ce03d699289675385
-        </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    myBtnEmail: {
-        marginHorizontal: 10,
-        borderColor: 'grey',
-        borderRadius: 3,
-        paddingHorizontal: 10,
-        paddingVertical: 5,
-        backgroundColor: '#e7e7e7'
-    },
-    myBtnEmailRight: {
-        marginHorizontal: 10,
-        borderColor: 'grey',
-        borderRadius: 3,
-        paddingHorizontal: 10,
-        paddingVertical: 5,
-        backgroundColor: '#e7e7e7'
-    },
-    container: {
-        flex: 1,
-        marginTop: 20
-    },
-    myInput: {
-        height: 50,
-        marginVertical: 20,
-        marginHorizontal: 20
-    },
-    myButton: {
-        marginHorizontal: 20,
-        marginVertical: 20,
-        paddingVertical: 5
-    },
-    myText: {
-        marginHorizontal: 20,
-        fontWeight: 'bold',
-        fontSize: 24,
-        color: 'black',
-        marginVertical: 20
-    },
-    myParagraph: {
-        marginHorizontal: 20,
-        marginVertical: 10,
-    }
-});
-
+    const styles = StyleSheet.create({
+        myBtnEmail: {
+            marginHorizontal: 10,
+            borderColor: 'grey',
+            borderRadius: 3,
+            paddingHorizontal: 10,
+            paddingVertical: 5,
+            backgroundColor: '#e7e7e7'
+        },
+        myBtnEmailRight: {
+            marginHorizontal: 10,
+            borderColor: 'grey',
+            borderRadius: 3,
+            paddingHorizontal: 10,
+            paddingVertical: 5,
+            backgroundColor: '#e7e7e7'
+        },
+        container: {
+            flex: 1,
+            marginTop: 20
+        },
+        myInput: {
+            height: 50,
+            marginVertical: 20,
+            marginHorizontal: 20
+        },
+        myButton: {
+            marginHorizontal: 20,
+            marginVertical: 20,
+            paddingVertical: 5
+        },
+        myText: {
+            marginHorizontal: 20,
+            fontWeight: 'bold',
+            fontSize: 24,
+            color: 'black',
+            marginVertical: 20
+        },
+        myParagraph: {
+            marginHorizontal: 20,
+            marginVertical: 10,
+        }
+    });
 export default AuthComponent;
--- src/components/AuthForgotPass.js
+++ src/components/AuthForgotPass.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import {Text} from "react-native-paper";
-import {View} from "react-native";
+import {StyleSheet,View} from 'react-native'
+import AuthProps from "./AuthProps";
+import {Button} from "react-native-paper";
 
 const AuthForgotPass = () => {
     return (
         <>
-
+            <AuthProps pass={false} email={'Электронная почта'}/>
+            <Button style={styles.BtnEmail} mode="contained">Далее</Button>
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    BtnEmail: {
+        marginVertical: 20,
+        marginHorizontal: 20
+    }
+});
 
 export default AuthForgotPass;
