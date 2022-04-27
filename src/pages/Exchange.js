3ec81fb081efcf70a2d2db05224226ee2a273200
Bayel <bayel.durusbekov.kk@gmail.com>
2022-04-27 13:42:34 +0600

    tab screen finish

---
+++ src/pages/Exchange.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from "react-native";
+
+function Exchange() {
+    return (
+        <Text>
+            Exchange
+        </Text>
+    );
+}
+
+export default Exchange;
